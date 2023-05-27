826679cced04762fcecb78ea6cfd74a12cfef15c
rodygayz456 <134363057+rodygayz456@users.noreply.github.com>
2023-05-27 16:27:54 +0500

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config(object):
     
     API_HASH = os.environ.get("API_HASH", "ca6186c6553ffe7db21f8f0b67765337") # Get this value from https://my.telegram.org/apps
     
-    OWNER_ID = int(os.environ.get("OWNER_ID", None)) # Your(owner's) telegram id
+    OWNER_ID = int(os.environ.get("OWNER_ID", "1287792972")) # Your(owner's) telegram id
     
     MONGO_STR = os.environ.get("MONGO_STR", "mongodb+srv://umar786:umar786@cluster0.rpuio0h.mongodb.net/?retryWrites=true&w=majority") # Get from MongoDB Atlas
 

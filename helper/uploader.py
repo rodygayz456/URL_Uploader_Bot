dfcdd3d6d914a48e45420ab86c746adf804aff80
rodygayz456 <134363057+rodygayz456@users.noreply.github.com>
2023-06-08 08:34:15 -0400

    Update uploader.py

--- helper/uploader.py
+++ helper/uploader.py
@@ -20,7 +20,7 @@ class Upload:
         self.userid = update.chat.id
         self.msg_id = update.message_id
         self.old_msg = old_msg
-        self.filename = 123.mp4
+        self.filename = filename
         self.downloadFolder = downloadFolder
 
     async def start(self):
